5e18fa33948e959fcbb88d8889d0912df5fe7e70
Keerthana Bellam <keerthanabellam@Keerthanas-MacBook-Air.local>
2026-06-03 14:32:33 -0400

    Fix Gemini model

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.write(
 # -----------------------------
 try:
     genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-    model = genai.GenerativeModel("gemini-1.5-flash-latest")
+    model = genai.GenerativeModel("gemini-2.0-flash")
 except:
     model = None
 
