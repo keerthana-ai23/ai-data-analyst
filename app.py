681105c5cc04c23a601d52d7e7567bb5871a5ca3
Keerthana Bellam <keerthanabellam@Keerthanas-MacBook-Air.local>
2026-06-04 10:52:48 -0400

    Fix upload logic

--- app.py
+++ app.py
@@ -27,6 +27,11 @@ if uploaded_file is not None:
     numeric_df = df.select_dtypes(
         include=["int64", "float64"]
     )
+else:
+st.info(
+    "Upload a CSV file to begin analysis."
+)
+
 
 # ====================================
 # DATASET HEALTH SCORE
@@ -261,9 +266,5 @@ st.download_button(
     data=report,
     file_name="analysis_report.csv",
     mime="text/csv"
-)
-else:
-st.info(
-    "Upload a CSV file to begin analysis."
 )
 
